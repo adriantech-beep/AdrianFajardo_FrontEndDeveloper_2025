75b84d64b28607fa9575eea1a3cb2f4a139483b3
adriantech-beep <Adhrianne29@gmail.com>
2025-05-20 18:51:43 +0800

    fix/updated-cv-link

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -100,7 +100,7 @@ function Main() {
         <SocialGroup />
 
         <StyledCvLink
-          href="https://drive.google.com/file/d/1K4b_zbnBqToCsgTok-qAI87ZNIC40jZ0/view?usp=drive_link"
+          href="https://drive.google.com/file/d/1LPJh3667X8YvUzut6TOXRFxe1OTVZal7/view?usp=drive_link"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Download Adrian Fajardo's Resume"
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -71,13 +71,89 @@ const StyledNav = styled.nav`
     }
   }
 `;
+const Span = styled.span`
+  .loader {
+    transform: rotateZ(45deg);
+    perspective: 1000px;
+    border-radius: 50%;
+    width: 48px;
+    height: 48px;
+    color: #fff;
+  }
+  .loader:before,
+  .loader:after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: inherit;
+    height: inherit;
+    border-radius: 50%;
+    transform: rotateX(70deg);
+    animation: 1s spin linear infinite;
+  }
+  .loader:after {
+    color: #ff3d00;
+    transform: rotateY(70deg);
+    animation-delay: 0.4s;
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: translate(-50%, -50%) rotateZ(0deg);
+    }
+    100% {
+      transform: translate(-50%, -50%) rotateZ(360deg);
+    }
+  }
+
+  @keyframes rotateccw {
+    0% {
+      transform: translate(-50%, -50%) rotate(0deg);
+    }
+    100% {
+      transform: translate(-50%, -50%) rotate(-360deg);
+    }
+  }
+
+  @keyframes spin {
+    0%,
+    100% {
+      box-shadow: 0.2em 0px 0 0px currentcolor;
+    }
+    12% {
+      box-shadow: 0.2em 0.2em 0 0 currentcolor;
+    }
+    25% {
+      box-shadow: 0 0.2em 0 0px currentcolor;
+    }
+    37% {
+      box-shadow: -0.2em 0.2em 0 0 currentcolor;
+    }
+    50% {
+      box-shadow: -0.2em 0 0 0 currentcolor;
+    }
+    62% {
+      box-shadow: -0.2em -0.2em 0 0 currentcolor;
+    }
+    75% {
+      box-shadow: 0px -0.2em 0 0 currentcolor;
+    }
+    87% {
+      box-shadow: 0.2em -0.2em 0 0 currentcolor;
+    }
+  }
+`;
 
 function Navigation() {
   const { scrollToProject, scrollToHome, scrollToAbout, setShow, show } =
     useScroll();
   return (
     <StyledNav>
-      <h1 onClick={scrollToHome}>Adrian</h1>
+      <h1 onClick={scrollToHome}>
+        Adrian <Span></Span>
+      </h1>
 
       <ul>
         <li onClick={scrollToHome}>Home</li>
